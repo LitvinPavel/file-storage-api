8ac707bb4ac06100ae24f9d283659870cefa51d8
Pavel <litvin.pv@gmail.com>
2021-03-01 01:15:33 +0300

    make it better

--- routes/file.js
+++ routes/file.js
@@ -71,7 +71,7 @@ router.post('/files', singleUpload, (req, res) => {
 });
 
 router.delete('/files/:id', (req, res) => {
-   gfs.remove({ _id: req.params.id }, (err) => {
+   gfs.remove({ _id: req.params.id, root: 'file-uploads' }, (err) => {
       if (err) return res.status(500).json({ success: false })
       return res.json({ success: true });
    })
